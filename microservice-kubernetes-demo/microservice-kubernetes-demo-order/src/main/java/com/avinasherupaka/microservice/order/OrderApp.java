d69529bb6fd44d6450e696f4f9f7b47e4de3acf4
avinasherupaka <avinash.jvm10@gmail.com>
2018-12-13 13:05:19 -0600

    intial working code

---
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-order/src/main/java/com/avinasherupaka/microservice/order/OrderApp.java
@@ -0,0 +1,15 @@
+package com.avinasherupaka.microservice.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class OrderApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApp.class, args);
+	}
+
+}
